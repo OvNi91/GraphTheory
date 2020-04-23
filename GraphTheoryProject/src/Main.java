16c204f59d1f29e2cf9327be920f919973cd625a
MedRoy <mederic.royer@efrei.net>
2020-04-23 16:41:54 +0200

    Changement du main, test d'une manière pour que ce soit claire.

--- GraphTheoryProject/src/Main.java
+++ GraphTheoryProject/src/Main.java
@@ -1,3 +1,4 @@
+import javax.sound.midi.Soundbank;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -14,43 +15,91 @@ public class Main {
             g.fillGraph(g.chosenGraph);
 
 
-            if (g.isCyclic(choice)) {
-                System.out.println("-> The graph has cycle <-");
-            } else {
-                System.out.println("-> The graph has no cycle <-");
-            }
 
-            g.setVerticesRank(choice);
+            System.out.println(">> ADJACENCY MATRIX <<");
+            System.out.println();
 
-            if (g.isScheduling(choice))
-            {
-                System.out.println(">> Graph is a correct graph scheduling <<");
-            } else {
-                System.out.println(">> The graph is not a correct graph scheduling <<");
-            }
+            g.displayAdjacencyMatrix();
+
+            System.out.println();
 
             System.out.println("---------------------------------------");
 
-
-            for (int i = 0; i < g.getNbVertices(); i++) {
-                System.out.println("Vertex " + g.listOfVertices.get(i).getVertexID() + " -> Rank : " + g.getListOfVertices().get(i).getVertexRank());
-                if (g.getListOfVertices().get(i).isSource()){
-                    System.out.println("This vertex is a source");
+            if (g.isCyclic(choice)) {
+                System.out.println("-> The graph " + choice + " has cycle <-");
+                if (g.isScheduling(choice))
+                {
+                    System.out.println(">> Graph is a correct graph scheduling <<");
+                } else {
+                    System.out.println(">> The graph is not a correct graph scheduling <<");
                 }
-                for (int j = 0; j < g.getListOfVertices().get(i).getListOfIngoingEdges().size(); j++) {
-                    System.out.println("Receives from : " + g.getListOfVertices().get(i).getListOfIngoingEdges().get(j).getEdgeChild().getVertexID() + " with a weight of : " + g.getListOfVertices().get(i).getListOfIngoingEdges().get(j).getEdgeWeight());
-                }
-
-                for (int j = 0; j < g.getListOfVertices().get(i).getListOfOutgoingEdges().size(); j++) {
-                    System.out.println("Sends to : " + g.getListOfVertices().get(i).getListOfOutgoingEdges().get(j).getEdgeParent().getVertexID() + " with a weight of : " + g.getListOfVertices().get(i).getListOfOutgoingEdges().get(j).getEdgeWeight());
-                }
-                System.out.println("-------------------------");
+            } else {
+                System.out.println(">> The graph " + choice + " has no cycle <<");
+                System.out.println(">> The graph is not a correct graph scheduling <<");
+                g.setVerticesRank(choice);
             }
+
+
+            System.out.println("---------------------------------------");
+
+           if (seeRanksInMain())
+           {
+               if (g.isCyclic(choice))
+               {
+                   System.out.println("The graph is cyclic, therefore we cannot see the ranks of the vertices");
+               } else {
+                   for (int i = 0; i < g.getNbVertices(); i++) {
+                       System.out.println("Vertex " + g.listOfVertices.get(i).getVertexID() + " -> Rank : " + g.getListOfVertices().get(i).getVertexRank());
+                       System.out.println();
+                       if (g.getListOfVertices().get(i).isSource()){
+                           System.out.println("This vertex is a source");
+                       }
+                       for (int j = 0; j < g.getListOfVertices().get(i).getListOfIngoingEdges().size(); j++) {
+                           System.out.println("Receives from : " + g.getListOfVertices().get(i).getListOfIngoingEdges().get(j).getEdgeChild().getVertexID() + " with a weight of : " + g.getListOfVertices().get(i).getListOfIngoingEdges().get(j).getEdgeWeight());
+                       }
+
+                       for (int j = 0; j < g.getListOfVertices().get(i).getListOfOutgoingEdges().size(); j++) {
+                           System.out.println("Sends to : " + g.getListOfVertices().get(i).getListOfOutgoingEdges().get(j).getEdgeParent().getVertexID() + " with a weight of : " + g.getListOfVertices().get(i).getListOfOutgoingEdges().get(j).getEdgeWeight());
+                       }
+                       System.out.println("-------------------------");
+                   }
+               }
+
+           }
+
             System.out.println("-------------------------");
+            System.out.println();
             System.out.println("Give another number between 1 and 13 if you want to see another graph, or anything else to exit");
             choice = sc.nextInt();
         }
         System.out.println("Bye !");
+    }
+
+
+
+    public static boolean seeRanksInMain()
+    {
+        Scanner sc = new Scanner(System.in);
+        int choice2;
+
+        System.out.println("Would you like to see the ranks of the vertices \n" +
+                "1 - Yes\n" +
+                "2 - No ");
+        choice2 = sc.nextInt();
+
+        while (choice2 != 1 && choice2 != 2)
+        {
+            System.out.println("Would you like to see the ranks of the vertices \n" +
+                    "1 - Yes\n" +
+                    "2 - No ");
+            choice2 = sc.nextInt();
+        }
+
+        if (choice2 == 1)
+            return true;
+        if (choice2 == 2)
+            return false;
+        return true;
     }
 }
 
